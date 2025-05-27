4b5ab303119fcac12a34cff1b1f69c3b579760da
combine143 <combine142@gmail.com>
2025-05-27 13:23:50 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+
+const headerFooterRows = table.querySelectorAll('thead tr, tfoot tr');
+
+headerFooterRows.forEach((row) => {
+  const th = document.createElement('th');
+
+  th.textContent = 'Position';
+  row.insertBefore(th, row.children[4]);
+});
+
+const rows = table.querySelectorAll('tbody tr');
+
+rows.forEach((row) => {
+  const position = row.cells[1].textContent;
+
+  const td = document.createElement('td');
+
+  td.textContent = position;
+  row.insertBefore(td, row.children[4]);
+});
